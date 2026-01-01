57de8aa92e11aa76ced0c77635fe764c76699286
Sharkai256 <justasharky@i.ua>
2026-01-01 16:56:58 +0200

    Continue

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Message from "./components/Message";
 import ListGroup from "./components/ListGroup";
+import Images from "./components/Images";
 
 const App = () => {
 	return <div>
 		<Message></Message>
+		<Images></Images>
 		<ListGroup></ListGroup>
 	</div>;
 }
---
+++ src/components/Images.tsx
@@ -0,0 +1,5 @@
+const Images = () => {
+	return <h1>images are here</h1>;
+}
+
+export default Images;
