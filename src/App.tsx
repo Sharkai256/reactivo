64d70642061e9dc7f77b1cf68713fdd2392941af
Sharkai <justasharky@i.ua>
2026-02-23 19:00:46 +0200

    Changed to grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { useState, useEffect } from "react";
-import ImageList from "./components/ImageList";
 import Toggle from "./components/Toggle"
-import Button from "./components/Button";
 import './App.css'
 
 const persistedState = (key: string, defaultValue: boolean) => {
@@ -20,20 +18,20 @@ const persistedState = (key: string, defaultValue: boolean) => {
 const App = () => {
 	let [darkTheme, setTheme] = persistedState("darkTheme", false);
 
-	const images = [
-		["src/assets/img/mavi_1.png", "src/assets/img/mavi_2.png", "src/assets/sfx/terraria_fem_hit.mp3"],
-		["src/assets/img/bn_4.png", "src/assets/img/bn_2.png", "src/assets/sfx/hit.mp3"],
-		["src/assets/img/rivulet_2.png", "src/assets/img/rivulet_1.png", "src/assets/sfx/pluh.mp3"]
-	];
-
 	return	<div className="main outer" color-theme={darkTheme ? "dark" : "white"}>
-		<ImageList
-			images={images}
-			className={"inner"}>
-			Silly images
-		</ImageList>
+		<div className="item">
+			<img src="./src/assets/img/mavi_1.png"/>
+			<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi rem fuga veniam magni. Molestiae rem vitae eos ipsa distinctio tenetur officiis suscipit. Molestias, repudiandae quis atque amet consectetur delectus cupiditate.</p>
+		</div>
+		<div className="item">
+			<img src="./src/assets/img/rivulet_1.png"/>
+			<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi rem fuga veniam magni. Molestiae rem vitae eos ipsa distinctio tenetur officiis suscipit. Molestias, repudiandae quis atque amet consectetur delectus cupiditate.</p>
+		</div>
+		<div className="item">
+			<img src="./src/assets/img/bn_2.png"/>
+			<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi rem fuga veniam magni. Molestiae rem vitae eos ipsa distinctio tenetur officiis suscipit. Molestias, repudiandae quis atque amet consectetur delectus cupiditate.</p>
+		</div>
 		<Toggle theme={darkTheme} className="toggle" onChange={() => setTheme(!darkTheme)}></Toggle>
-		<Button className="evil" sound="src/assets/sfx/evil_laugh.mp3">magick button</Button>
 	</div>
 }
 
